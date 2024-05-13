446632ee303abeea2bb3c11d9a0bbf3ce2be2565
safalthapa77 <safalthapa77@kbc.edu.np>
2024-05-13 12:30:57 +0545

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      body
+    <Footer />
+    </div>
+  )
+}
+
+export default App
